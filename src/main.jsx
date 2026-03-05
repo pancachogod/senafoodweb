b24d1adc3b83f1ecea44d60466f7fa3af13b5ab6
pancachogod <dfc3045@gmail.com>
2026-03-04 19:59:20 -0500

    remove custom cursor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import clickCursor from './assets/punteroi.png';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { CartProvider } from './context/CartContext.jsx';
 import { OrdersProvider } from './context/OrdersContext.jsx';
 import './index.css';
-
-if (typeof document !== 'undefined') {
-  document.documentElement.style.setProperty(
-    '--cursor-interactive',
-    `url(${clickCursor}) 8 2, auto`
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
